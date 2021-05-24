2a9df10c1cffa57465ef86bce645883844c4ef2a
Simon Genin (ges) <ges@odoo.com>
2021-05-24 18:04:02 +0200

    wip

--- src/popup/components/pupup_app.ts
+++ src/popup/components/pupup_app.ts
@@ -2,10 +2,32 @@ import { Component, tags } from "@odoo/owl";
 
 const TEMPLATE = tags.xml/*xml*/ `
     <div t-name="PopUpApp">
-      Hello popup !
+        <header class="flex justify-center items-baseline mt-4">
+            <img class="h-12" src="../assets/images/odoo_logo.png" />
+        </header>              
+
+        <div class="mt-4 bg-gray-100">
+            <div class="p-4">
+                <h1 class="text-2xl">Debug mode</h1>
+                <div class="flex justify-around items-center mt-2">
+                    <button class="px-4 py-2 uppercase font-bold text-sm rounded" t-attf-class="{{ currentMode === 'PROD' ? activeClasses : nonActiveClasses }}" type="button">Prod</button>
+                    <button class="px-4 py-2 uppercase font-bold text-sm rounded" t-attf-class="{{ currentMode === 'TESTS' ? activeClasses : nonActiveClasses }}" type="button">Tests</button>
+                    <button class="px-4 py-2 uppercase font-bold text-sm rounded" t-attf-class="{{ currentMode === 'ASSETS' ? activeClasses : nonActiveClasses }}" type="button">Assets</button>
+                </div>
+            </div>
+        </div>
+
     </div>
 `;
 
 export class PopUpApp extends Component {
     static template = TEMPLATE;
+
+    static modes = ["PROD", "TESTS", "ASSETS"];
+    static defaultMode = "PROD";
+
+    activeClasses = "bg-primary cursor-not-allowed text-white"
+    nonActiveClasses = "bg-primary-200 hover:bg-primary-300"
+
+    currentMode: string = PopUpApp.defaultMode
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-    future: {
-        // removeDeprecatedGapUtilities: true,
-        // purgeLayersByDefault: true,
-    },
-    purge: false, // purging handled manually
+    mode: "jit",
+    purge: ["./src/**/*.{js,ts,xml}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#865A7B",
+                    50: "#E8DDE6",
+                    100: "#DECEDA",
+                    200: "#CAAFC3",
+                    300: "#B591AC",
+                    400: "#A17295",
+                    500: "#865A7B",
+                    600: "#67465F",
+                    700: "#493143",
+                    800: "#2A1D27",
+                    900: "#0C080B",
+                },
+            },
+        },
     },
     variants: {},
     plugins: [],
